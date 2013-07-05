25197f7da486fd6b759b26d4587c325211723bb0
Sławosz <slawosz@gmail.com>
2013-07-05 18:58:23 +0200

    initial commit

---
+++ src/handlers.go
@@ -0,0 +1,47 @@
+// Implement the following types and define ServeHTTP methods on them. Register them to handle specific paths in your web server.
+//
+// type String string
+//
+// type Struct struct {
+//     Greeting string
+//     Punct    string
+//     Who      string
+// }
+// For example, you should be able to register handlers using:
+//
+// http.Handle("/string", String("I'm a frayed knot."))
+// http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+
+package main
+
+import (
+    "net/http"
+    "fmt"
+)
+
+type String string
+
+type Struct struct {
+    Greeting string
+    Punct    string
+    Who      string
+}
+
+func (s String) ServeHTTP(
+    w http.ResponseWriter,
+    r *http.Request) {
+    fmt.Fprint(w, s)
+}
+
+func (s Struct) ServeHTTP(
+    w http.ResponseWriter,
+    r *http.Request) {
+    fmt.Fprint(w, s)
+}
+
+func main() {
+    http.Handle("/string", String("I'm a frayed knot."))
+    http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+
+    http.ListenAndServe("localhost:4000", nil)
+}
